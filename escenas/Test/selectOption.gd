ec0a9a438b31f15a27858a6d4353f4a8b20ea364
Alacesp <1c.alvaroacedoespejo@uma.es>
2025-03-12 13:47:56 +0100

    Preguntas pre añadidas al csv

---
+++ escenas/Test/selectOption.gd
@@ -0,0 +1,6 @@
+extends Button
+func _on_mouse_entered():
+	get_parent().modulate.a = 0.85
+
+func _on_mouse_exited():
+	get_parent().modulate.a = 1
---
+++ escenas/Test/text_edit.gd
@@ -0,0 +1,4 @@
+extends TextEdit
+
+#func _init():
+#	self.background_color = Color(0.862745, 0.0784314, 0.235294, 1)
