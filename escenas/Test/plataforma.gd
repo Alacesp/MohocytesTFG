0ffe9a80bd8148a6e30af202aac0de39588e451e
Alacesp <1c.alvaroacedoespejo@uma.es>
2025-03-19 14:45:29 +0100

    drag and drop medio hecho

--- escenas/Test/plataforma.gd
+++ escenas/Test/plataforma.gd
@@ -1,7 +1,5 @@
 extends StaticBody2D
 
-var occupied_by: Node2D = null  # Keep track of which object is on this platform
-
 func _ready():
 	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
 
@@ -10,13 +8,3 @@ func _process(delta):
 		visible = true
 	else:
 		visible = false
-
-# Function to add an object to the platform
-func _add_object(obj: Node2D) -> void:
-	occupied_by = obj
-	obj.position = position  # Ensure the object is placed correctly on the platform
-
-# Function to remove an object from the platform
-func _remove_object(obj: Node2D) -> void:
-	if occupied_by == obj:
-		occupied_by = null
