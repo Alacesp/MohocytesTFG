2714c9cf0dd1d9da8fff1d00ac41461aa5b25a5d
Alacesp <1c.alvaroacedoespejo@uma.es>
2025-04-13 19:47:02 +0200

    base del prototipo de wet, vistas y botones

---
+++ escenas/juegoWetLabSegundo/movimiento.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@onready
+var camara = get_node("../camara")
+
+
+func _on_boton_1_pressed() -> void:
+	camara.position.x = 1728
+
+
+func _on_boton_2_pressed() -> void:
+	camara.position.x = 576
+
+
+func _on_boton_3_pressed() -> void:
+	camara.position.x = 2880
+
+
+func _on_boton_4_pressed() -> void:
+	camara.position.x = 1728
+
+
+func _on_boton_5_pressed() -> void:
+	camara.position.x = 4032
+
+
+func _on_boton_6_pressed() -> void:
+	camara.position.x = 2880
