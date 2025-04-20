ca5bdc4037f7238066c8f905510f37a5b8772fd0
Alacesp <1c.alvaroacedoespejo@uma.es>
2025-04-20 14:05:02 +0200

    animacion inventario

---
+++ escenas/juegoWetLabSegundo/botoninventario.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+@onready
+var inventario = get_parent()
+var closed = true
+
+func _on_button_pressed() -> void:
+	var tween = get_tree().create_tween()
+	if closed:
+		tween.tween_property(inventario, "position", Vector2(233,124), 0.5 ).set_trans(Tween.TRANS_LINEAR)
+		closed = false
+	else:
+		tween.tween_property(inventario, "position", Vector2(233,654), 0.5 ).set_trans(Tween.TRANS_LINEAR)
+		closed = true
