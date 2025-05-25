08305e36e97621c7ed2390e403adb0cad1297f9b
Alacesp <1c.alvaroacedoespejo@uma.es>
2025-05-25 15:02:13 +0200

    iconos caras y bocadillos wet

--- escenas/juegoWetLabSegundo/ManejadorDeClientes.gd
+++ escenas/juegoWetLabSegundo/ManejadorDeClientes.gd
@@ -103,7 +103,7 @@ func _on_timer_timeout() -> void:
 		"Happy":
 			$"Cliente1".visible=true
 			$"../camara/Clientes/Cliente1".visible = true
-			$"../camara/Clientes/Cliente1".texture = preload("res://recursos/happySymbol.png")
+			$"../camara/Clientes/Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlContentaSymbol.png")
 			estado1 = "Serious"
 			$"Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlContenta.png")
 			$"Cliente1/Timer".start(20)
@@ -111,23 +111,23 @@ func _on_timer_timeout() -> void:
 			estado1 = "Sad"
 			$"Cliente1/Timer".start(10)
 			$"Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlSeria.png")
-			$"../camara/Clientes/Cliente1".texture = preload("res://recursos/seriousSymbol.png")
+			$"../camara/Clientes/Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlSeriaSymbol.png")
 		"Sad":
 			estado1 = "Angry"
 			$"Cliente1/Timer".start(20)
 			$"Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlTriste.png")
-			$"../camara/Clientes/Cliente1".texture = preload("res://recursos/sadSymbol.png")
+			$"../camara/Clientes/Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlTristeSymbol.png")
 		"Angry":
 			estado1 = "Happy"
 			$"Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlEnfadada.png")
-			$"../camara/Clientes/Cliente1".texture = preload("res://recursos/angrySymbol.png")
+			$"../camara/Clientes/Cliente1".texture = preload("res://escenas/juegoWetLabSegundo/sprites/jwlEnfadadaSymbol.png")
 
 func _on_timer_2_timeout() -> void:
 	match estado2:
 		"Happy":
 			$"Cliente2".visible=true
 			$"../camara/Clientes/Cliente2".visible = true
-			$"../camara/Clientes/Cliente2".texture = preload("res://recursos/happySymbol.png")
+			$"../camara/Clientes/Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1ContentoSymbol.png")
 			estado2 = "Serious"
 			$"Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1Contento.png")
 			$"Cliente2/Timer2".start(20)
@@ -135,23 +135,23 @@ func _on_timer_2_timeout() -> void:
 			estado2 = "Sad"
 			$"Cliente2/Timer2".start(10)
 			$"Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1Serio.png")
-			$"../camara/Clientes/Cliente2".texture = preload("res://recursos/seriousSymbol.png")
+			$"../camara/Clientes/Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1SerioSymbol.png")
 		"Sad":
 			estado2 = "Angry"
 			$"Cliente2/Timer2".start(20)
 			$"Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1Triste.png")
-			$"../camara/Clientes/Cliente2".texture = preload("res://recursos/sadSymbol.png")
+			$"../camara/Clientes/Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1TristeSymbol.png")
 		"Angry":
 			estado2 = "Happy"
 			$"Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1Enfadado.png")
-			$"../camara/Clientes/Cliente2".texture = preload("res://recursos/angrySymbol.png")
+			$"../camara/Clientes/Cliente2".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e1EnfadadoSymbol.png")
 
 func _on_timer_3_timeout() -> void:
 	match estado3:
 		"Happy":
 			$"Cliente3".visible=true
 			$"../camara/Clientes/Cliente3".visible = true
-			$"../camara/Clientes/Cliente3".texture = preload("res://recursos/happySymbol.png")
+			$"../camara/Clientes/Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2ContentoSymbol.png")
 			estado3 = "Serious"
 			$"Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2Contento.png")
 			$"Cliente3/Timer3".start(20)
@@ -159,23 +159,23 @@ func _on_timer_3_timeout() -> void:
 			estado3 = "Sad"
 			$"Cliente3/Timer3".start(10)
 			$"Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2Serio.png")
-			$"../camara/Clientes/Cliente3".texture = preload("res://recursos/seriousSymbol.png")
+			$"../camara/Clientes/Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2SerioSymbol.png")
 		"Sad":
 			estado3 = "Angry"
 			$"Cliente3/Timer3".start(20)
 			$"Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2Triste.png")
-			$"../camara/Clientes/Cliente3".texture = preload("res://recursos/sadSymbol.png")
+			$"../camara/Clientes/Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2TristeSymbol.png")
 		"Angry":
 			estado3 = "Happy"
 			$"Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2Enfadado.png")
-			$"../camara/Clientes/Cliente3".texture = preload("res://recursos/angrySymbol.png")
+			$"../camara/Clientes/Cliente3".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e2EnfadadoSymbol.png")
 
 func _on_timer_4_timeout() -> void:
 	match estado4:
 		"Happy":
 			$"Cliente4".visible=true
 			$"../camara/Clientes/Cliente4".visible = true
-			$"../camara/Clientes/Cliente4".texture = preload("res://recursos/happySymbol.png")
+			$"../camara/Clientes/Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3ContentoSymbol.png")
 			estado4 = "Serious"
 			$"Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3Contento.png")
 			$"Cliente4/Timer4".start(20)
@@ -183,13 +183,13 @@ func _on_timer_4_timeout() -> void:
 			estado4 = "Sad"
 			$"Cliente4/Timer4".start(10)
 			$"Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3Serio.png")
-			$"../camara/Clientes/Cliente4".texture = preload("res://recursos/seriousSymbol.png")
+			$"../camara/Clientes/Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3SerioSymbol.png")
 		"Sad":
 			estado4 = "Angry"
 			$"Cliente4/Timer4".start(20)
 			$"Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3Triste.png")
-			$"../camara/Clientes/Cliente4".texture = preload("res://recursos/sadSymbol.png")
+			$"../camara/Clientes/Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3TristeSymbol.png")
 		"Angry":
 			estado4 = "Happy"
 			$"Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3Enfadado.png")
-			$"../camara/Clientes/Cliente4".texture = preload("res://recursos/angrySymbol.png")
+			$"../camara/Clientes/Cliente4".texture = preload("res://escenas/juegoWetLabSegundo/sprites/e3EnfadadoSymbol.png")
